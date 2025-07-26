b129eb450b3a1a193239eb28b5e01fb4a1e53e0a
Mohammed Zeeshan <93642091+alizeeshan1234@users.noreply.github.com>
2025-07-26 06:13:51 +0530

    Update mod.rs

--- src/instructions/mod.rs
+++ src/instructions/mod.rs
@@ -15,25 +15,3 @@ pub use init_proposal::*;
 pub mod process_vote;
 pub use process_vote::*;
 
-// pub enum MultisigInstructions {
-//     InitializeMultisig = 0,
-//     AddMember = 1,
-//     RemoveMember = 2,
-//     InitializeProposal = 3,
-//     Vote = 4,
-// }
-
-// impl TryFrom<&u8> for MultisigInstructions {
-//     type Error = ProgramError;
-
-//     fn try_from(value: &u8) -> Result<Self, Self::Error> {
-//         match value {
-//             0 => Ok(MultisigInstructions::InitializeMultisig),
-//             1 => Ok(MultisigInstructions::AddMember),
-//             2 => Ok(MultisigInstructions::RemoveMember),
-//             3 => Ok(MultisigInstructions::InitializeProposal),
-//             4 => Ok(MultisigInstructions::Vote),
-//             _ => Err(ProgramError::InvalidInstructionData)
-//         }
-//     }
-// }
